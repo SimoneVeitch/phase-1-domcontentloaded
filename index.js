4e508ed7ff813b41d31815dbef03018424bac3b5
Simone Veitch <simone.veitch@gmail.com>
2024-03-07 14:12:11 +1100

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const para = this.getElementById ('text');
+    para.textContent = "This is really cool!";
+  });
+  
